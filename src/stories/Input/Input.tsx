4a5dec060c5aa45716f0cb4e629d9a676f7ad337
miguelrp99 <miguelrodriguez.p99@gmail.com>
2024-06-21 18:41:30 +0200

    Input styles partially added

--- src/stories/Input/Input.tsx
+++ src/stories/Input/Input.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { InputRadius, InputSizes, InputVariants } from "./constants";
+import styles from "./input.module.css";
 
 type Props = {
-    label?: string;
+    placeholder?: string;
     size?: InputSizes;
     variant?: InputVariants;
     radius?: InputRadius;
 }
-export const Input = ({ label }: Props) => {
+export const Input = ({ placeholder = "" }: Props) => {
     return (
-        <div>
-            {label}
+        <div className={styles["input-container"]}>
+            <input id="email" className={`${styles.input} ${styles.peer}`} placeholder={placeholder} />
+            <label htmlFor="email" className={styles.label}>Email</label>
         </div>
     );
 }
