be16e1876678273a54cab4bf40a4ef4ef2d4eee5
Miguel Rodriguez Perez <miguel.rodriguezp@udc.es>
2024-07-05 22:17:02 +0200

    [Project] Cleaned app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
 import React from "react";
 
-import Dropdown, {
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-} from "./stories/Dropdown";
-import { Button } from "./stories";
-import { NewFileIcon } from "./Icons/NewFileIcon";
-import { CopyLinkIcon } from "./Icons/CopyLinkIcon";
-import { EditFileIcon } from "./Icons/EditFileIcon";
-import { DeleteFileIcon } from "./Icons/DeleteFileIcon";
-
 function App() {
   return (
     <div
@@ -21,82 +10,7 @@ function App() {
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
-      <Dropdown>
-        <DropdownTrigger>
-          <Button radius="md" variant="bordered" color="default" blackText>
-            Open Menu
-          </Button>
-        </DropdownTrigger>
-        <DropdownMenu aria-label="Static Actions">
-          <DropdownItem
-            startContent={<NewFileIcon />}
-            color="primary"
-            key="new"
-          >
-            New file
-          </DropdownItem>
-          <DropdownItem
-            startContent={<CopyLinkIcon />}
-            color="default"
-            key="copy"
-          >
-            Copy link
-          </DropdownItem>
-          <DropdownItem
-            startContent={<EditFileIcon />}
-            color="success"
-            key="edit"
-          >
-            Edit file
-          </DropdownItem>
-          <DropdownItem
-            startContent={<DeleteFileIcon />}
-            key="delete"
-            color="danger"
-            variant="solid"
-          >
-            Delete file
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-
-      {/* <Dropdown> with endContent*/}
-      <Dropdown>
-        <DropdownTrigger>
-          <Button radius="md" variant="bordered" color="default" blackText>
-            Open Menu
-          </Button>
-        </DropdownTrigger>
-        <DropdownMenu aria-label="Static Actions">
-          <DropdownItem endContent={<NewFileIcon />} color="primary" key="new">
-            New file
-          </DropdownItem>
-          <DropdownItem
-            endContent={<CopyLinkIcon />}
-            color="default"
-            key="copy"
-          >
-            Copy link
-          </DropdownItem>
-          <DropdownItem
-            endContent={<EditFileIcon />}
-            color="success"
-            key="edit"
-          >
-            Edit file
-          </DropdownItem>
-          <DropdownItem
-            endContent={<DeleteFileIcon />}
-            key="delete"
-            color="danger"
-            variant="solid"
-          >
-            Delete file
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    </div>
+    ></div>
   );
 }
 
