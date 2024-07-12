f3969abba04ebe217d349540da8f826a4b224e00
Miguel Rodriguez Perez <miguel.rodriguezp@udc.es>
2024-07-12 17:20:25 +0200

    [Avatar] Fixed story

--- src/stories/Avatar/Avatar.stories.tsx
+++ src/stories/Avatar/Avatar.stories.tsx
@@ -1,88 +1,118 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Avatar } from './Avatar';
-import { AvatarGroup as Group } from './AvatarGroup'; // Renamed the import to resolve the conflict
-import { radii, sizes } from './constants';
-import { semanticColors } from '../../lib/colors';
-import React from 'react';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Avatar } from "./Avatar";
+import { AvatarGroup as Group } from "./AvatarGroup"; // Renamed the import to resolve the conflict
+import { radii, sizes } from "./constants";
+import { semanticColors } from "../../lib/colors";
+import React from "react";
 
 const meta: Meta<typeof Avatar> = {
-    title: 'Components/Avatar',
-    component: Avatar,
-    argTypes: {
-        size: {
-            options: Object.values(sizes),
-            control: { type: 'select' },
-        },
-        color: {
-            options: Object.values(semanticColors),
-            control: { type: 'select' },
-        },
-        radius: {
-            options: Object.values(radii),
-            control: { type: 'select' },
-        },
+  title: "Components/Avatar",
+  component: Avatar,
+  argTypes: {
+    size: {
+      options: Object.values(sizes),
+      control: { type: "select" },
     },
+    color: {
+      options: Object.values(semanticColors),
+      control: { type: "select" },
+    },
+    radius: {
+      options: Object.values(radii),
+      control: { type: "select" },
+    },
+  },
 };
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-    },
+  args: {},
 };
 
 export const WithText: Story = {
-    args: {
-        name: "John Doe",
-    },
+  args: {
+    name: "John Doe",
+  },
 };
 
 export const WithImage: Story = {
-    args: {
-        avatar: "./src/stories/assets/hero-card-complete.jpeg",
-    },
+  args: {
+    avatar:
+      "https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
 };
 
 export const WithBorder: Story = {
-    args: {
-        bordered: true,
-    },
+  args: {
+    bordered: true,
+  },
 };
 
 export const Disabled: Story = {
-    args: {
-        isDisabled: true,
-    },
+  args: {
+    isDisabled: true,
+  },
 };
 
 export const AvatarGroupHorizontal: Story = {
-    render: () => (
-        <Group animation='horizontal'> {/* Use the renamed import */}
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </Group>
-    )
-}
+  render: () => (
+    <Group animation="horizontal">
+      {" "}
+      {/* Use the renamed import */}
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+    </Group>
+  ),
+};
 
 export const AvatarGroupVertical: Story = {
-    render: () => (
-        <Group animation='vertical'> {/* Use the renamed import */}
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </Group>
-    )
-}
-
+  render: () => (
+    <Group animation="vertical">
+      {" "}
+      {/* Use the renamed import */}
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+    </Group>
+  ),
+};
 
 export const AvatarGroupDisabled: Story = {
-    render: () => (
-        <Group animation='horizontal' isDisabled>
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-            <Avatar bordered avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </Group>
-    )
-}
+  render: () => (
+    <Group animation="horizontal" isDisabled>
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Avatar
+        bordered
+        avatar="https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+    </Group>
+  ),
+};
