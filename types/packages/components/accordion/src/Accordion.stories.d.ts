8f218ad425b42f2f51c731dab6ba25d67dc533fc
miguelrp99 <miguelrodriguez.p99@gmail.com>
2024-07-19 21:52:17 +0200

    [Project] Build updated

--- types/packages/components/accordion/src/Accordion.stories.d.ts
+++ types/packages/components/accordion/src/Accordion.stories.d.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Accordion } from "@mirakle-ui/accordion";
+import Accordion from "./Accordion";
 import { AccordionSelectionModes, AccordionVariants } from "./constants";
 interface AccordionArgs {
     variant: AccordionVariants;
