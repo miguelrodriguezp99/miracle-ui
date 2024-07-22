12500845872aa9f38965a8b3ba00dc588b8185db
miguelrp99 <miguelrodriguez.p99@gmail.com>
2024-07-23 01:36:46 +0200

    [Input] Fixed position styles

--- packages/App.tsx
+++ packages/App.tsx
@@ -2,21 +2,80 @@ import React from "react";
 import "./components/styles/global.css";
 import "./app.css";
 
-import { Input } from "./components";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "./components";
+import { SpainFlag } from "./icons/SpainFlag";
 
 function App() {
   return (
     <>
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
+          gap: "10rem",
           backgroundColor: "black",
           height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
         }}
       >
-        <Input variant="underlined" />
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              radius="md"
+              variant="bordered"
+              color="default"
+              blackText={false}
+            ></Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            position="bottom-end"
+            offset={5}
+            aria-label="Static Actions"
+          >
+            <DropdownItem key="new">
+              New file to make people happy!
+            </DropdownItem>
+            <DropdownItem key="copy">Copy link</DropdownItem>
+            <DropdownItem key="edit">Edit file</DropdownItem>
+            <DropdownItem key="delete" color="danger" variant="solid">
+              Delete file
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              radius="md"
+              variant="bordered"
+              color="default"
+              blackText={false}
+              isIconOnly
+            >
+              <SpainFlag />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            position="bottom-end"
+            offset={5}
+            aria-label="Static Actions"
+          >
+            <DropdownItem key="new">New file</DropdownItem>
+
+            <DropdownItem key="copy">Copy link</DropdownItem>
+            <DropdownItem key="edit">Edit file</DropdownItem>
+            <DropdownItem key="delete" color="danger" variant="solid">
+              Delete file
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     </>
   );
--- packages/components/dropdown/src/components/DropdownMenu.tsx
+++ packages/components/dropdown/src/components/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import classNames from "classnames";
 import DropdownContext from "../DropdownContext";
 import styles from "./../dropdown.module.css";
@@ -13,78 +13,105 @@ interface DropdownMenuProps {
   "aria-label"?: string;
 }
 
-const useDropdownStyle = ({
-  position,
-  offset,
-  width,
-  backgroundColor,
-}: {
-  position: DropdownPositions;
-  offset: number;
-  width: number;
-  backgroundColor?: string;
-}) => {
-  const style: React.CSSProperties = { width: `${width}px`, backgroundColor };
-
-  const marginOffset = `${offset}px`;
-
-  switch (position) {
-    case positions.bottom:
-    case positions["bottom-start"]:
-    case positions["bottom-end"]:
-      style.marginTop = marginOffset;
-      break;
-    case positions.top:
-    case positions["top-start"]:
-    case positions["top-end"]:
-      style.marginBottom = marginOffset;
-      break;
-    case positions.right:
-    case positions["right-start"]:
-    case positions["right-end"]:
-      style.marginLeft = marginOffset;
-      break;
-    case positions.left:
-    case positions["left-start"]:
-    case positions["left-end"]:
-      style.marginRight = marginOffset;
-      break;
-  }
-
-  return style;
-};
-
 export const DropdownMenu = ({
   children,
   position = positions.bottom,
-  offset = 0,
+  offset = 10,
   width = 200,
   backgroundColor,
 }: DropdownMenuProps) => {
   const { isOpen } = useContext(DropdownContext);
-  const style = useDropdownStyle({ position, offset, width, backgroundColor });
+  const listWrapperRef = useRef<HTMLDivElement>(null);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    if (listWrapperRef.current) {
+      const { offsetWidth, offsetHeight } = listWrapperRef.current;
+      setDimensions({ width: offsetWidth, height: offsetHeight });
+    }
+  }, [dimensions]);
 
   return (
     <div
-      style={style}
+      id="listWrapper"
+      ref={listWrapperRef}
+      style={{
+        width: `${width}px`,
+        backgroundColor: backgroundColor,
+        marginBottom:
+          position === positions.top ||
+          position === positions["top-end"] ||
+          position === positions["top-start"]
+            ? `${offset}px`
+            : "",
+        marginRight:
+          position === positions.left ||
+          position === positions["left-end"] ||
+          position === positions["left-start"]
+            ? `${offset}px`
+            : "",
+        marginLeft:
+          position === positions.right ||
+          position === positions["right-end"] ||
+          position === positions["right-start"]
+            ? `${offset}px`
+            : "",
+        marginTop:
+          position === positions.bottom ||
+          position === positions["bottom-end"] ||
+          position === positions["bottom-start"]
+            ? `${offset}px`
+            : "",
+
+        transform:
+          isOpen &&
+          (position === positions["top-start"] ||
+            position === positions["bottom-start"])
+            ? `translateX(-${dimensions.width * 0.4}px) scale(1)`
+            : isOpen &&
+                (position === positions["top-end"] ||
+                  position === positions["bottom-end"])
+              ? `translateX(${-dimensions.width * 0.6}px) scale(1)`
+              : isOpen &&
+                  (position === positions["left-start"] ||
+                    position === positions["right-start"])
+                ? `translateY(-${dimensions.height * 0.4}px) scale(1)`
+                : isOpen &&
+                    (position === positions["left-end"] ||
+                      position === positions["right-end"])
+                  ? `translateY(${-dimensions.height * 0.6}px) scale(1)`
+                  : "",
+
+        left:
+          position === positions["top-start"] ||
+          position === positions["bottom-start"]
+            ? dimensions.width * 0.4
+            : "",
+
+        right:
+          position === positions["top-end"] ||
+          position === positions["bottom-end"]
+            ? -(dimensions.width * 0.6)
+            : "",
+
+        top:
+          position === positions["left-start"] ||
+          position === positions["right-start"]
+            ? dimensions.height * 0.4
+            : "",
+
+        bottom:
+          position === positions["left-end"] ||
+          position === positions["right-end"]
+            ? -dimensions.height * 0.6
+            : "",
+      }}
       className={classNames(styles.listWrapper, styles[position], {
         [styles.menuOpen]: isOpen,
-        [styles["top-transform"]]:
-          position === positions.top ||
-          position === positions["top-start"] ||
-          position === positions["top-end"],
-        [styles["left-transform"]]:
-          position === positions.left ||
-          position === positions["left-start"] ||
-          position === positions["left-end"],
-        [styles["bottom-transform"]]:
-          position === positions.bottom ||
-          position === positions["bottom-start"] ||
-          position === positions["bottom-end"],
-        [styles["right-transform"]]:
-          position === positions.right ||
-          position === positions["right-start"] ||
-          position === positions["right-end"],
+        [styles["vertical-transform"]]:
+          position === positions.top || position === positions.bottom,
+        [styles["horizontal-transform"]]:
+          position === positions.left || position === positions.right,
       })}
     >
       <ul className={styles.list}>{children}</ul>
